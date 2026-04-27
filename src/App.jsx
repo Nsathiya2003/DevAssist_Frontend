12ab34666b5a860711c2d369088c461e83f82e4b
sathiya <nsathiya757@gmail.com>
2026-04-27 17:54:53 +0530

    toastify package installed and imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import MainLayout from './Layouts/MainLayout'
-import Dashboard from './pages/Dashboard'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./Layouts/MainLayout";
+import Dashboard from "./pages/Dashboard";
+import { ToastContainer, toast } from "react-toastify";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="dashboard" element={<Dashboard />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
+  );
 }
 
-export default App
+export default App;
