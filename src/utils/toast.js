1045c38970d6ced01a8efc0d81947209d70e9b44
sathiya <nsathiya757@gmail.com>
2026-04-27 17:52:30 +0530

    Customized toast styling function created

---
+++ src/utils/toast.js
@@ -0,0 +1,21 @@
+import { toast } from "react-toastify";
+
+export const showSuccess = (message) => {
+    toast.success(message, {
+        position:'top-right',
+        autoClose:2000,
+        hideProgressBar:true
+    })
+}
+
+export const showError = (message) => {
+    toast.error(message, {
+        position:'top-right',
+        autoClose:3000,
+        hideProgressBar:true
+    })
+}
+
+export const showInfo = (message) => {
+    toast.info(message)
+}
