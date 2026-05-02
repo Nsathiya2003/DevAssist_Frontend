f647d2309feae592d88f3c7ccd8d7b7294ebe72a
sathiya <nsathiya757@gmail.com>
2026-05-02 13:12:50 +0530

    Passing the user input to the api

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,7 @@
 import { Sparkles, ArrowUp } from "lucide-react";
+import { useState } from "react";
+import { showError } from "../utils/toast";
+import { createChat } from "../services/chat";
 
 const PrimaryTitle = [
   "Explain React hooks",
@@ -8,6 +11,24 @@ const PrimaryTitle = [
 ];
 
 export default function Dashboard() {
+  const [chat, setChat] = useState([]);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setChat(value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      let res = await createChat({
+        prompt: chat,
+      });
+      console.log("response is", res);
+    } catch (error) {
+      showError(error?.response?.data?.message || "Something went wrong");
+    }
+  };
+
   return (
     <main className="h-[calc(100vh-64px)] flex items-center justify-center bg-gray-50">
       <section className="w-full max-w-2xl px-4 text-center">
@@ -34,9 +55,16 @@ export default function Dashboard() {
             type="text"
             placeholder="Ask anything..."
             className="flex-1 px-3 py-3 text-sm outline-none text-gray-700"
+            onChange={(e) => handleChange(e)}
+            value={chat}
+            name="chat"
+            id="chat"
           />
 
-          <button className="flex items-center justify-center w-10 h-10 rounded-xl cursor-pointer bg-black text-white hover:bg-gray-800 transition">
+          <button
+            className="flex items-center justify-center w-10 h-10 rounded-xl cursor-pointer bg-black text-white hover:bg-gray-800 transition"
+            onClick={handleSubmit}
+          >
             <ArrowUp className="w-4 h-4" />
           </button>
         </div>
